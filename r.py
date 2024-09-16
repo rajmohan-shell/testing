615bad86dfba9fbf0100dd771811a58c92bc19f3
Rajmohan <rajmohan.badiger@shell.com>
2024-09-16 16:51:29 +0530

    tst

---
+++ r.py
@@ -0,0 +1,10 @@
+x = range (11,51)
+for n in x:
+    if n % 5 == 0:
+        print ('number is devisibal by 5 '+ str(n))
+    elif n % 3 == 0:
+        print ('number is devisibal by 3 '+ str(n))
+
+        
+
+ 
